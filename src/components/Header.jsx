a0ad250995c0f6bc9ff923660281484488c80017
Sanalemba991 <154108599+Sanalemba991@users.noreply.github.com>
2024-07-01 22:17:32 +0530

    <a> Convert to <Link>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,21 @@
 import React from 'react';
-import Creater from './Creater'; // Make sure to import Creater component if you are using it
+import { Link } from 'react-router-dom';
+import Creater from './Creater'; 
 
-import {
-    createBrowserRouter,
-    Outlet,
-    RouterProvider,
-  } from "react-router-dom";
 export default function Header() {
-    const router=createBrowserRouter([{
-        path:"/creater",
-        element:<Creater/>
-
-    }])
     return (
         <header className='flex items-center justify-between gap-4 p-4'>
-            <a href="/"><h1 className='font-medium'>TransCribe<span className='text-orange-400 bold'>Translation</span></h1></a>
+            <Link to="/" className='text-black'>
+                <h1 className='font-medium'>
+                    TransCribe<span className='text-orange-400'>Translation</span>
+                </h1>
+            </Link>
             <div className='gap-4 flex items-center'>
-                
-                <a href="/creater" className='flex items-center gap-2 specialBtn px-3 py-2 rounded-lg text-orange-400'>
-                    <button onClick={router}></button>
+                <Link to="/creater" className='flex items-center gap-2 specialBtn px-3 py-2 rounded-lg text-orange-400'>
                     <p>New</p>
-                    <a href="/creater" target='_blank' className='text-amber-600 ml-7 cursor-pointer' rel="noreferrer">Created Detais</a>
+                    <span className='text-amber-600 ml-7 cursor-pointer'>Created Details</span>
                     <i className="fa-solid fa-plus"></i>
-                </a>
+                </Link>
             </div>
         </header>
     );
